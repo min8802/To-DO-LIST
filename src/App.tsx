f8dae3792c452fb548a15d8855f09bc9b811f4dc
min8802 <ww330@naver.com>
2024-05-29 14:54:42 +0900

    sampleData edit

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,32 @@ import TodoList from "./components/TodoList";
 const sampleData: ITodo[] = [
   {
       id: 1,
-      content: "달리기",
+      content: "점심 요리",
       isDone: false,
   },
   {
       id: 2,
-      content: "풍선구매",
+      content: "설거지",
+      isDone: false,
+  },
+  {
+      id: 3,
+      content: "코딩",
+      isDone: false,
+  },
+  {
+      id: 4,
+      content: "저녁 요리",
+      isDone: false,
+  },
+  {
+      id: 5,
+      content: "설거지",
+      isDone: false,
+  },
+  {
+      id: 6,
+      content: "코딩",
       isDone: false,
   },
 ];
@@ -20,7 +40,7 @@ const sampleData: ITodo[] = [
 const App: FC = () => {
   const [todos, setTodos] = useState<ITodo[]>(sampleData);
   
-  
+
   return <Flex flexDir="column" minH="100vh">
     <CreateTodo todos={todos} setTodos={setTodos}/>
     <TodoList todos={todos}/>
